27b10bed62a8a41c13af44d1e916a235f93c464c
Wolfram Kriesing <wk@uxebu.com>
2010-08-29 22:50:04 +0200

    Everything that is dojo compatible put in a dojo object, use embedjs one day ....

--- js/_global.js
+++ js/_global.js
@@ -86,9 +86,22 @@ var util = {
 		}
 		return ret;
 	},
-	
+}
+
+// Our mini dojo ... API compatible for the features we use :)
+// Would like to build this set as we need it down here using embedJS some day ... will be possible, just needs the time to do it :)
+globalNamespace = this;
+var dojo = {
 	hitch:function(scope, method){
 		return function(){ return scope[method].apply(scope, arguments || []); };
+	},
+	declare:function(name, parent, klass){
+		globalNamespace[name] = klass["constructor"] || function(){};
+		var ref = globalNamespace[name];
+		for (var prop in klass){
+			if (prop=="constructor") continue;
+			ref.prototype[prop] = klass[prop];
+		}
 	},
 };
 
